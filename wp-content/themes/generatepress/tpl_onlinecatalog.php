c07c71d4fa3258499131a9f6488e5cfa9c84a5f0
maxistepanov <stepanovonline@gmail.com>
2017-11-08 22:25:33 +0200

    fix url

--- wp-content/themes/generatepress/tpl_onlinecatalog.php
+++ wp-content/themes/generatepress/tpl_onlinecatalog.php
@@ -46,10 +46,10 @@ get_header(); ?>
 				
 				<div class="entry-content" itemprop="text">
 					<?php 
-		// $serverName = "DESKTOP-IJOBITJ\SQLEXPRESS"; 
+    // $serverName = "DESKTOP-IJOBITJ\SQLEXPRESS"; 
+// $connectionInfo = array( "Database"=>"library", "CharacterSet" => "UTF-8");
 $serverName = "10.2.81.252";
-    
-$connectionInfo = array( "Database"=>"library", "CharacterSet" => "UTF-8");
+$connectionInfo = array( "Database"=>"library",'UID'=>'Chitatel', 'PWD'=>'katalog', "CharacterSet" => "UTF-8");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 if( $conn === false ) {
     die( print_r( sqlsrv_errors(), true));
