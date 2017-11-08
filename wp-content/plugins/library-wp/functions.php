3eaaed59ecfd1bce3e02c2a40411c70d14172ac7
maxistepanov <stepanovonline@gmail.com>
2017-11-08 22:20:12 +0200

    fix url

--- wp-content/plugins/library-wp/functions.php
+++ wp-content/plugins/library-wp/functions.php
@@ -27,14 +27,14 @@ function hneu_scripts() {
     }
 function get_connection()
 {
-	$serverName = "DESKTOP-IJOBITJ\SQLEXPRESS"; 
-	$connectionInfo = array( "Database"=>"library", "CharacterSet" => "UTF-8");
-	$conn = sqlsrv_connect( $serverName, $connectionInfo);
+	// $serverName = "DESKTOP-IJOBITJ\SQLEXPRESS"; 
+	// $connectionInfo = array( "Database"=>"library", "CharacterSet" => "UTF-8");
+	// $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 	
-// $serverName = "10.2.81.252";
-// $connectionInfo = array( "Database"=>"library",'UID'=>'Chitatel', 'PWD'=>'katalog', "CharacterSet" => "UTF-8");
-// $conn = sqlsrv_connect( $serverName, $connectionInfo);
+$serverName = "10.2.81.252";
+$connectionInfo = array( "Database"=>"library",'UID'=>'Chitatel', 'PWD'=>'katalog', "CharacterSet" => "UTF-8");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
 	
 	return $conn;
 }
