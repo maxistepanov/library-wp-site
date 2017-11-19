09631ce1642151190871c865fc3e98778085b11d
maxistepanov <stepanovonline@gmail.com>
2017-11-19 10:32:34 +0200

    add bd config

--- wp-content/themes/generatepress/tpl_onlinecatalog.php
+++ wp-content/themes/generatepress/tpl_onlinecatalog.php
@@ -47,12 +47,9 @@ get_header(); ?>
 				
 				<div class="entry-content" itemprop="text">
 					<?php 
-    // $serverName = "DESKTOP-IJOBITJ\SQLEXPRESS"; 
-// $connectionInfo = array( "Database"=>"library", "CharacterSet" => "UTF-8");
-// $serverName = "10.2.81.252";
-$serverName = "library.ch5bf5k9ozoc.us-east-2.rds.amazonaws.com";
-$connectionInfo = array( "Database"=>"library",'UID'=>'maxi', 'PWD'=>'novatel720', "CharacterSet" => "UTF-8");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+  $conn = get_connection();      
+
 
 if( $conn === false ) {
     die( print_r( sqlsrv_errors(), true));
