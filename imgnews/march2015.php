4b43096bbcf95bece7f1a29547b27db4847cf5ed
Maxi Stepanov <stepanovonline@gmail.com>
2017-12-03 15:10:36 +0200

    add source image

---
+++ imgnews/march2015.php
@@ -0,0 +1,141 @@
+<html>
+<head>
+
+<title>Знаменні дати: Березень | Бібліотека ХНЕУ</title>
+<meta name="Author" content="">
+<meta name="Keywords" content="Бібліотека, ХНЕУ">
+<meta name="Description" content="Офіційний сайт бібліотеки ХНЕУ">
+
+<meta http-equiv="content-type" content="text/html; charset=windows-1251">
+
+<link href="./files/gen.css" type="text/css" rel="stylesheet">
+
+		<!--jquery-->	
+		<script type="text/javascript" src="jq/jquery-1.5.1.min.js"></script>
+		<script type="text/javascript" src="jq/jquery-ui-1.8.14.custom.min.js"></script>
+		<!--slides-->
+		<script type="text/javascript" src="slideshow.js"></script>
+	
+		
+<script type="text/javascript">
+
+function overName(element)
+{ 
+ element.style.background="#c8e0f1";
+}
+function outName(element)
+{
+element.style.background="transparent";
+}
+
+
+function overDiv(element)
+{ 
+ element.style.background="white";
+}
+function outDiv(element)
+{
+element.style.background="#c8e0f1";
+}
+
+function klik(el)
+{
+location.href="http://b2.ru/news.php?news="+el.id;
+//window.open("http://b.ru/news.php?news="+el.id);
+//alert(element.id);
+
+
+//.write('<div align="center" style="width: 80%; position:fixed; height: 80%; background: red;">');
+//document.write('</div>');
+
+}
+
+</SCRIPT>
+
+</head>
+
+<body>
+
+<table align="center" valign="top" width="100%" cellspacing="0" cellpadding="0" border="0">
+<tr>
+<td align="center" valign="top">
+<div style="width:950px; text-align:center;  background:white;">
+<!--<img src="files/text.jpg"></div>-->
+<div id="ramka" style="width: 950px; position:relative; height: 260px; background: url('files/rama.png') no-repeat; text-align:left; ">
+<!--slideshow-->
+<?include 'slideshow.php';?>
+</div>
+</td>
+</tr>	
+	
+<tr>
+<td>
+
+<table width="950px" align="center" valign="center" cellspacing="0" cellpadding="0" border="0">
+		<tr>
+		<td bgcolor="white">
+			<table cellspacing="0" cellpadding="0" border="0" align="center">
+				<tr align="center" >
+					<td valign="top" >
+			<!--menu-->
+			<?include 'menu.php';?>
+					</td>
+				</tr>
+			</table>
+		
+		</td>
+	</tr>
+		
+		
+	
+	<tr>
+		<td>
+
+			<table width="100%" cellspacing="0" cellpadding="0" border="0">
+
+		<!-- Основная часть -->
+				<tr>
+				
+				<td colspan="3" bgcolor="white">
+				
+				<ul id="crumbs">
+		<li>Знаменні дати: Березень</li>
+		<!--<li><a href="#"  >Категория</a></li>
+		<li><a href="#"  >Раздел</a></li>
+		<li><a href="#"  >Подраздел</a></li>
+		<li>Текущая страница</li>-->
+	</ul>
+				
+				</td>
+				
+				</tr>
+			<tr>
+				
+				<td colspan="3" bgcolor="white">
+				<?include 'march2015.html';?>
+				</td
+			</tr>
+				
+				<tr>
+				<td>
+				<!-- Низ страницы -->
+				<table width="100%" cellspacing="0" cellpadding="0" border="0">
+
+				<tr>
+				<td align="center" valign="top" bgcolor="white">
+				<hr size="2px" align="center" width="95%" color="#c8e0f1">
+				<div style="height:30px; text-align:center;"><b>Бібліотека ХНЕУ</b></div>
+				</td>
+				</tr>
+			</table>
+
+</td>
+</tr>
+</table>
+
+</td>
+</tr>
+</table>
+
+</body>
+</html>
