3f398a9a88db4ed4020b2f9aa3270b4e2d16b7b8
maxistepanov <stepanovonline@gmail.com>
2017-12-25 23:51:18 +0200

    fix db url

--- wp-content/plugins/library-wp/bdconnectionconfig.php
+++ wp-content/plugins/library-wp/bdconnectionconfig.php
@@ -7,12 +7,12 @@ function get_connection()
 	
 
 //rds
-$serverName = "library.ch5bf5k9ozoc.us-east-2.rds.amazonaws.com";
-$connectionInfo = array( "Database"=>"library",'UID'=>'maxi', 'PWD'=>'novatel720', "CharacterSet" => "UTF-8");
+// $serverName = "library.ch5bf5k9ozoc.us-east-2.rds.amazonaws.com";
+// $connectionInfo = array( "Database"=>"library",'UID'=>'maxi', 'PWD'=>'novatel720', "CharacterSet" => "UTF-8");
 	
 	// live
- // $serverName = "10.2.81.252";
- // $connectionInfo = array( "Database"=>"library",'UID'=>'Chitatel', 'PWD'=>'katalog', "CharacterSet" => "UTF-8");
+ $serverName = "10.2.81.252";
+ $connectionInfo = array( "Database"=>"library",'UID'=>'Chitatel', 'PWD'=>'katalog', "CharacterSet" => "UTF-8");
 
 	$conn = sqlsrv_connect( $serverName, $connectionInfo);
 	return $conn;
